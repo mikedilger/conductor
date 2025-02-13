7c9f1224b75b5dd91564b43c14fc26ac51de68d8
Mike Dilger <mike@mikedilger.com>
2025-02-14 10:38:48 +1300

    Further on running commands on relay

--- src/nip86.rs
+++ src/nip86.rs
@@ -1,27 +1,35 @@
+use base64::Engine;
+use http::uri::{Scheme, Uri};
 use nostr::event::{Event, Kind, Tags, Tag, TagStandard, UnsignedEvent};
 use nostr::nips::nip07::BrowserSigner;
 use nostr::nips::nip98::HttpMethod;
 use nostr::signer::NostrSigner;
 use nostr::types::time::Timestamp;
-use nostr::types::url::RelayUrl;
+use nostr::util::{hex, JsonUtil};
+use reqwest::Client;
+use secp256k1::hashes::{sha256, Hash};
+use serde_json::{json, Value};
 
-// Create auth event
+async fn auth_header(uri: &Uri, payload: &str)
+                         -> Result<String, Box<dyn std::error::Error>>
+{
+    let payload_hash = sha256::Hash::hash(payload.as_bytes());
+    let payload_hash_bytes = <sha256::Hash as AsRef<[u8]>>::as_ref(&payload_hash);
+    let payload_hash_string = hex::encode(payload_hash_bytes);
+    let event = auth_event(uri, &payload_hash_string).await?;
+    let base64 = base64::engine::general_purpose::STANDARD.encode(event.as_json());
+    Ok(format!("Nostr {}", base64))
+}
 
-//
-// content-type: application/nostr+json+rpc
-// authorization: (nip-98)
-//    payload tag is required, u tag is the relay url
-
-
-pub async fn auth(relay_url: RelayUrl, method: HttpMethod, payload: &str)
+async fn auth_event(uri: &Uri, payload_hash: &str)
                   -> Result<Event, Box<dyn std::error::Error>>
 {
     let signer = BrowserSigner::new()?;
 
     let tags = Tags::new(vec![
-        TagStandard::Url(relay_url.try_into().unwrap()).into(),
-        TagStandard::Method(method).into(),
-        Tag::parse(["payload", payload])?,
+        Tag::parse(["u", &format!("{}", uri)])?,
+        Tag::parse(["payload", payload_hash])?,
+        TagStandard::Method(HttpMethod::POST).into(),
     ]);
 
     let unsigned = UnsignedEvent {
@@ -36,4 +44,67 @@ pub async fn auth(relay_url: RelayUrl, method: HttpMethod, payload: &str)
     let event = signer.sign_event(unsigned).await?;
 
     Ok(event)
+}
+
+async fn post(uri: &Uri, body: String) -> Result<Value, Box<dyn std::error::Error>> {
+    let host = {
+        let authority = uri.authority().expect("Has no hostname").as_str();
+        authority
+            .find('@')
+            .map(|idx| authority.split_at(idx + 1).1)
+            .unwrap_or_else(|| authority)
+            .to_owned()
+    };
+    let mut parts = uri.clone().into_parts();
+    if host.is_empty() {
+        panic!("URL has empty hostname");
+    }
+    parts.scheme = match parts
+        .scheme
+        .ok_or(std::io::Error::other("Missing scheme"))?
+        .as_str()
+    {
+        "wss" => Some(Scheme::HTTPS),
+        "ws" => Some(Scheme::HTTP),
+        "https" => Some(Scheme::HTTPS),
+        "http" => Some(Scheme::HTTP),
+        _ => panic!("We don't support that scheme."),
+    };
+    let uri = Uri::from_parts(parts)?;
+
+    let auth = auth_header(&uri, &body).await?;
+
+    let client = Client::builder()
+        .build()?;
+    let response = client
+        .post(format!("{}", uri))
+        .header("Host", host)
+        .header("Accept", "application/nostr+json+rpc")
+        .header("Authorization", auth)
+        .body(body)
+        .send().await?;
+
+    let status = response.status().as_u16();
+    let response_text = response.text().await?;
+    if status != 200 {
+        return Err(Box::new(std::io::Error::other(format!("Server responded with {}", status))));
+    }
+
+    let value: Value = serde_json::from_str(&response_text)?;
+    Ok(value)
+}
+
+pub async fn run_command_on_relay(
+    url: &str,
+    method: &str,
+    params: Value
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let cmd = json!({
+        "method": method,
+        "params": params
+    });
+    let cmdstr = serde_json::to_string(&cmd)?;
+    let uri = url.parse::<Uri>()?;
+    let value = post(&uri, cmdstr).await?;
+    Ok(value)
 }
