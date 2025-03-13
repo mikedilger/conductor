4d0bac96b496f3af1cb63e8a8ffe247b67a15c18
Mike Dilger <mike@mikedilger.com>
2025-03-14 12:46:42 +1300

    Attempt simple use of FetchJob [DashMap parking_lot panic means deadlock]

--- src/nostr.rs
+++ src/nostr.rs
@@ -1,3 +1,4 @@
+use crate::fetch_job::FetchJob;
 use lazy_static::lazy_static;
 use dashmap::DashMap;
 use nostr::event::id::EventId;
@@ -11,13 +12,103 @@ use serde_json::Value;
 use std::time::Duration;
 
 lazy_static! {
-    static ref METADATA: DashMap<PublicKey, Option<Metadata>> = {
-        DashMap::new()
-    };
-
     static ref CLIENTS: DashMap<String, NostrClient> = {
         DashMap::new()
     };
+
+    static ref METADATA: DashMap<PublicKey, FetchJob<Option<Metadata>>> = {
+        DashMap::new()
+    };
+
+}
+
+pub async fn get_metadata(pubkey: PublicKey, discovery_relay_url: String) ->
+    Result<Option<Metadata>, Box<dyn std::error::Error>>
+{
+    match METADATA.get(&pubkey) {
+        Some(fj) => Ok(fj.get().await),
+        None => {
+            let fj = FetchJob::new();
+            METADATA.insert(pubkey, fj.clone());
+            fetch_metadata(pubkey, discovery_relay_url, fj).await?;
+            match METADATA.get(&pubkey) {
+                Some(fj) => Ok(fj.get().await),
+                None => panic!("Impossible"),
+            }
+        }
+    }
+}
+
+pub async fn fetch_metadata(
+    pubkey: PublicKey,
+    discovery_relay_url: String,
+    fj: FetchJob<Option<Metadata>>
+) -> Result<(), Box<dyn std::error::Error>> {
+    let filter: Filter = Filter::default()
+        .author(pubkey)
+        .kind(Kind::RelayList);
+    let events = fetch_events(&discovery_relay_url, filter).await?;
+    if events.is_empty() {
+        fj.complete(None);
+        return Ok(());
+    }
+
+    // collect 'r' tags that have no marker or that have a 'read' marker.
+    let mut relays: Vec<String> = vec![];
+    for tag in events[0].tags.iter().cloned() {
+        let fields = tag.to_vec();
+        if fields.first() == Some(&"r".to_string()) {
+            if let Some(url) = fields.get(1) {
+                if fields.get(2).is_none() || fields.get(2) == Some(&"read".to_owned()) {
+                    relays.push(url.to_owned());
+                }
+            }
+        }
+    }
+    if relays.is_empty() {
+        fj.complete(None);
+        return Ok(());
+    }
+
+    // get metadata
+    let filter: Filter = Filter::default()
+        .author(pubkey)
+        .kind(Kind::Metadata);
+    let events = fetch_events(&relays[0], filter).await?;
+    if events.is_empty() {
+        fj.complete(None);
+        return Ok(());
+    }
+
+    let metadata: Metadata = serde_json::from_str(&events[0].content)?;
+    fj.complete(Some(metadata));
+    Ok(())
+}
+
+pub async fn fetch_events(
+    url: &str,
+    filter: Filter,
+) -> Result<Vec<Event>, Box<dyn std::error::Error>> {
+    if filter.is_empty() {
+        return Ok(vec![]);
+    }
+
+    // Create client in CLIENTS if missing
+    if ! CLIENTS.contains_key(url) {
+        let client = NostrClient::default();
+        client.set_signer(BrowserSigner::new()?).await;
+        client.add_relay(url).await?;
+        CLIENTS.insert(url.to_owned(), client);
+    }
+
+    let client = CLIENTS.get(url).unwrap();
+    client.connect().await;
+    let events = client
+        .fetch_events(filter, Duration::from_secs(5))
+        .await?
+        .to_vec();
+
+    Ok(events)
 }
 
 pub fn id_list_to_filter(arr: Vec<Value>) -> Filter {
@@ -50,86 +141,4 @@ pub fn pubkey_list_to_vec(arr: Vec<Value>) -> Vec<PublicKey> {
         }
     }
     output
-}
-
-pub async fn get_metadata(pubkey: PublicKey, discovery_relay_url: String) ->
-    Result<Option<Metadata>, Box<dyn std::error::Error>>
-{
-    fetch_metadata(pubkey, discovery_relay_url).await
-}
-
-pub async fn fetch_events(
-    url: &str,
-    filter: Filter,
-) -> Result<Vec<Event>, Box<dyn std::error::Error>> {
-    if filter.is_empty() {
-        return Ok(vec![]);
-    }
-
-    // Create client in CLIENTS if missing
-    if ! CLIENTS.contains_key(url) {
-        let client = NostrClient::default();
-        client.set_signer(BrowserSigner::new()?).await;
-        client.add_relay(url).await?;
-        CLIENTS.insert(url.to_owned(), client);
-    }
-
-    let client = CLIENTS.get(url).unwrap();
-    client.connect().await;
-    let events = client
-        .fetch_events(filter, Duration::from_secs(5))
-        .await?
-        .to_vec();
-
-    Ok(events)
-}
-
-pub async fn fetch_metadata(pubkey: PublicKey, discovery_relay_url: String) ->
-    Result<Option<Metadata>, Box<dyn std::error::Error>>
-{
-    if let Some(optmd) = METADATA.get(&pubkey) {
-        return Ok(optmd.clone());
-    }
-
-    let filter: Filter = Filter::default()
-        .author(pubkey)
-        .kind(Kind::RelayList);
-    let events = fetch_events(&discovery_relay_url, filter).await?;
-    if events.is_empty() {
-        METADATA.insert(pubkey, None);
-        return Ok(None);
-    }
-
-    // collect 'r' tags that have no marker or that have a 'read' marker.
-    let mut relays: Vec<String> = vec![];
-    for tag in events[0].tags.iter().cloned() {
-        let fields = tag.to_vec();
-        if fields.first() == Some(&"r".to_string()) {
-            if let Some(url) = fields.get(1) {
-                if fields.get(2).is_none() || fields.get(2) == Some(&"read".to_owned()) {
-                    relays.push(url.to_owned());
-                }
-            }
-        }
-    }
-    if relays.is_empty() {
-        METADATA.insert(pubkey, None);
-        return Ok(None);
-    }
-
-    // get metadata
-    let filter: Filter = Filter::default()
-        .author(pubkey)
-        .kind(Kind::Metadata);
-    let events = fetch_events(&relays[0], filter).await?;
-    if events.is_empty() {
-        METADATA.insert(pubkey, None);
-        return Ok(None);
-    }
-
-    let metadata: Metadata = serde_json::from_str(&events[0].content)?;
-
-    METADATA.insert(pubkey, Some(metadata.clone()));
-
-    Ok(Some(metadata))
 }
